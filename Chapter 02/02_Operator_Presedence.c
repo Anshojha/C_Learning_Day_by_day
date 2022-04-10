fc4a5a5861ef1605e486b8913c5848ee3b3f35bb
Ansh Ojha <anshojha24@gmail.com>
2022-04-11 00:19:20 +0530

    Add files via upload

---
+++ Chapter 02/02_Operator_Presedence.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+int x = 4;
+int y = 9;
+printf("The Value of 3*x-8*y %d\n", 3*x-8*y);
+    return 0;
+}
---
+++ Chapter 02/_practice.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    printf("Enter the number:\n");
+    scanf("%d", &num);
+    printf("The dividibility test return %d\n", num % 97);
+    return 0;
+}
